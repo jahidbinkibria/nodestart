9e7145432067644595cdb3528b9f0cf0454ac628
jahid <jk311857@gmail.com>
2018-01-27 02:10:10 +0600

    node events and EventEmitter

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+var events = require("events"); 
+var myEmitter = new events.EventEmitter();
 
+myEmitter.on("someEvent", function (mesg) {
+    console.log(mesg);
+})
+myEmitter.emit("someEvent", "Thats an event");
